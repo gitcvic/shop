a825569a99259d31db311e631863eefa79774f9d
root <root@debian>
2014-10-26 03:53:17 +0000

    asdf

---
+++ src/Shop/PublicBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shop\PublicBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller{
+    
+    public function indexAction()
+    {
+        return new Response('hello world2', '200');
+    }
+    
+}
---
+++ src/Shop/PublicBundle/ShopPublicBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Shop\PublicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopPublicBundle extends Bundle
+{
+}
